8387b133147d47adfcde67adb65fb036b15e0332
Syther007 <null.0.node@protonmail.com>
2024-06-18 21:08:15 +1200

    Update with Proxy

--- script.sh
+++ script.sh
@@ -2,26 +2,35 @@
 
 # Specify the input file containing the URLs
 input_file="urls.txt"
+
 # Specify the output directory to save the downloaded files
 output_dir="downloads"
+
 # Set the timeout value (in seconds)
 timeout=30
+
 # Set the random delay range (in seconds)
-min_delay=5
-max_delay=60
+min_delay=1
+max_delay=5
+
 # Set the maximum number of retries for each download
 max_retries=3
+
 # Set the simulate_download variable to control the download behavior
 simulate_download=true
 
 # Check if the output directory exists, create it if not
 if [ ! -d "$output_dir" ]; then
     mkdir "$output_dir"
+    echo "Output directory created."
+else
+    echo "Output directory already exists."
 fi
 
 # Initialize variables to track the total downloaded size and count
 total_size=0
 download_count=0
+
 
 # Iterate through each URL in the input file
 while IFS= read -r url; do
@@ -33,19 +42,24 @@ while IFS= read -r url; do
 
     # Download the website assets using wget with a timeout, retries, and simulate_download
     if [ "$simulate_download" = true ]; then
-        wget --quiet --timeout="$timeout" --tries="$max_retries" -r -p -A "*.{html,pdf,doc,docx,txt,zip,jpg,jpeg,png,gif,bmp,css,js}" -O /dev/null "$url"
+        # wget --timeout="$timeout" --tries="$max_retries" --quiet -r -p -A "*.{html,pdf,doc,docx,txt,zip,jpg,jpeg,png,gif,bmp,css,js}" -O /dev/null "$url"
+        wget -e use_proxy=yes -e https_proxy=127.0.0.1:443 --timeout="$timeout" --tries="$max_retries" -O /dev/null "$url"
     else
-        wget --quiet --timeout="$timeout" --tries="$max_retries" -r -p -A "*.{html,pdf,doc,docx,txt,zip,jpg,jpeg,png,gif,bmp,css,js}" -O "$output_dir/$filename" "$url"
+        wget -e use_proxy=yes -e https_proxy=127.0.0.1:443 --timeout="$timeout" --tries="$max_retries" --quiet -r -p -A "*.{html,pdf,doc,docx,txt,zip,jpg,jpeg,png,gif,bmp,css,js}" -O "$output_dir/$filename" "$url"
     fi
 
     # Calculate the total downloaded size
-    file_size=$(stat -c%s "$output_dir/$filename")
-    total_size=$((total_size+file_size))
+    if [ "$simulate_download" = false ]; then
+        file_size=$(stat -c%s "$output_dir/$filename")
+        total_size=$((total_size+file_size))
+    fi
     download_count=$((download_count+1))
     echo "Downloaded $url (size: ${file_size} bytes)"
+
     # Print a progress report
     progress=$(echo "scale=2; ($download_count / $(wc -l < "$input_file")) * 100" | bc -l)
     echo "Progress: $progress% ($download_count/$(( $(wc -l < "$input_file") )))"
+
     # Add a random delay within the specified range
     sleep $((RANDOM % ($max_delay - $min_delay + 1) + $min_delay))
 done < "$input_file"

5f09ce618fbdf5b5f24d714a81e225f0b0f1ad71
Syther007 <null.0.node@protonmail.com>
2024-10-14 09:30:19 +1300

    Added Logging to lightCons_cron.sh

--- lightCons_cron.sh
+++ lightCons_cron.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
+# Create a log file and write the header information
+log_file="logs/lightCons_cron_log_$(date +%Y-%m-%d).log"  # Set the log file name to include the current date
+maxIterations=10  # Set the desired number of iterations
+
+
+
+
+{
+    echo "Log File Created: $(date)"
+    echo "Operating System: $(uname -s)"
+    echo "Hostname: $(hostname)"
+    echo "Local IP: $(hostname -I | awk '{print $1}')"
+    echo "Public IP: $(curl -s https://icanhazip.com)"
+    echo "----------------------------------------"
+} > "$log_file"
+
+# Start the timer
+start_time=$(date +%s)
+
 allThreads=(
        "https://www.asite.com" 
        "https://icanhazip.com" 
@@ -15,10 +34,10 @@ ArrCnt=0;
 SUC=0;
 FAL=0;
 TOL=0;
-maxIterations=50000  # Set the desired number of iterations
-currentIteration=0
 
 totalDownloaded=0  # Initialize total downloaded size in bytes
+currentIteration=0
+
 
 while (( currentIteration < maxIterations ));
 do
@@ -56,4 +75,16 @@ do
 done 
 
 # At the end of the script, display the total downloaded size
-echo "Final Total Downloaded: ${totalDownloadedMB} MB"
+echo "Final Total Downloaded: ${totalDownloadedMB} MB" | tee -a "$log_file"
+
+# Calculate and print the total execution time
+end_time=$(date +%s)
+execution_time=$((end_time - start_time))
+echo "Total Execution Time: $execution_time seconds" | tee -a "$log_file"
+
+# Append the last 20 lines of the output to the log file
+{
+    echo "----------------------------------------"
+    echo "Last 20 lines of output:"
+    tail -n 20 "$log_file"
+} >> "$log_file"
